c501fc1b3768152c7c7871157ac65adb0ea9044d
waterloo8675309 <gregfox380@gmail.com>
2025-04-17 19:31:22 -0500

    Scaffold modular i3 config with autostart, binds, theme, and swaylock effects

---
+++ config/i3/scripts/lockscreen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+swaylock \
+  --screenshots \
+  --clock \
+  --indicator \
+  --effect-blur 7x5 \
+  --effect-vignette 0.5:0.5 \
+  --ring-color 2d3748 \
+  --key-hl-color b4f9f8 \
+  --line-color 00000000 \
+  --inside-color 00000088 \
+  --separator-color 00000000 \
+  --grace 2 \
+  --fade-in 0.2
